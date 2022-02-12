a9da372a196cc0940516fc5bb209933386e405e5
EddieMCDev <99547997+EddieMC-Dev@users.noreply.github.com>
2022-02-12 19:42:30 -0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,4 +1,4 @@
 aux, fibo, quant = 1, 0, int(input('Quantidades de números na sequência: '))
 for cont in range(0, quant):
-   print(fibo)
+   print(fibo, end=', ' if cont != quant-1 else '.\n')
    fibo, aux = fibo+aux, fibo  
